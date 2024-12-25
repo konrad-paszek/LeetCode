757bf23f402b20124486752420becafe5508bbb7
konrad-paszek <konradpaszek.dev@gmail.com>
2024-12-25 22:06:23 +0100

    25.12.2024

---
+++ .venv/1342. Number of Steps to Reduce a Number to Zero.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        counter = 0
+        while num:
+            if num & 1:
+                num -= 1
+            else:
+                num >>= 1
+            counter +=1
+        return counter
