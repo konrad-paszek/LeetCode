e1ec5e7114161d352e698716ad971d4c00f22ad1
konrad-paszek <konradpaszek.dev@gmail.com>
2025-01-09 20:34:57 +0100

    09.01.2025

--- 7. Reverse Integer.py
+++ 7. Reverse Integer.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Solution:
     def reverse(self, x: int) -> int:
         reversed_string = str(x)[::-1]
@@ -8,3 +11,19 @@ class Solution:
         if -2**31 > int(result) or int(result) > 2**31:
             return 0
         return int(result)
+
+
+class Solution:
+    def reverse(self, x: int):
+        res = 0
+        while x:
+            digit = int(math.fmod(x ,10))
+            x = int(x / 10)
+            res = (res * 10) + digit
+            if res >= 2 ** 31 or res <= -2**31:
+                return 0
+        return res
+
+y = Solution()
+
+print(y.reverse(123))
